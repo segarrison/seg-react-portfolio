1459ce7aceaa50465db09f659a02848f7e733512
segarrison <79775636+segarrison@users.noreply.github.com>
2021-12-21 02:02:53 -0600

    contact me animation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import TextField from "@mui/material/TextField";
+import React from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Animated } from "react-animated-css";
 
 const theme = createTheme({
   typography: {
@@ -22,8 +22,12 @@ function Contact() {
       <Grid container justifyContent="center">
       
         <Box component="div">
+          
         <ThemeProvider theme={theme}>
+        <Animated animationIn="bounceInRight" animationInDuration={2000}>
           <Typography variant="h2" align="center" color={"#DC2F02"}><strong><br />Contact Me!</strong></Typography>
+          </Animated>
+          <Animated animationIn="bounceInLeft" animationInDuration={3000} animationInDelay={800}>
           <Typography variant="h4" align="center" color={"#F48C06"}><strong>
             <br />
             I am currently looking for freelance or full time work <br />
@@ -33,11 +37,15 @@ function Contact() {
             Additionally, if you'd like to collaborate on a project, <br />
             I would also love to hear from you!</strong>
           </Typography>
+          </Animated>
+          <Animated animationIn="bounceInUp" animationInDuration={2000} animationInDelay={3000}>
           <Typography align="center">
             <br />
           <Link variant="h4" href="mailto:s.garrison@gmail.com?subject=Let's Work Together!" color={"#9D0208"}>Let's get started!</Link>
           </Typography>
+          </Animated>
           </ThemeProvider>
+          
         </Box>
         
       </Grid>
