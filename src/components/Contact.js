0f5d4a6a9f5a140a043a1d436f5a9e7b2e3ca6de
segarrison <79775636+segarrison@users.noreply.github.com>
2021-12-21 01:49:47 -0600

    Typography for Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,31 +3,43 @@ import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import SendIcon from "@mui/icons-material";
-import { styled } from "@mui/material/styles";
-import Paper from "@mui/material/Paper";
+import Link from "@mui/material/Link";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      '"Josefin Sans"',
+      'sans-serif'
+    ].join(','),
+  }
+})
 
 function Contact() {
 
   return (
     <Box component="div">
       <Grid container justifyContent="center">
-      <Paper>
-        <Box component="form">
-          
-          <Typography variant="h5">Contact Me!</Typography>
-          <Typography variant="h6">
-            To actually get in touch with me:
+      
+        <Box component="div">
+        <ThemeProvider theme={theme}>
+          <Typography variant="h2" align="center" color={"#DC2F02"}><strong><br />Contact Me!</strong></Typography>
+          <Typography variant="h4" align="center" color={"#F48C06"}><strong>
+            <br />
+            I am currently looking for freelance or full time work <br />
+            in either a remote setting or in the Houston, TX area.
+            <br />
+            <br />
+            Additionally, if you'd like to collaborate on a project, <br />
+            I would also love to hear from you!</strong>
           </Typography>
-          <Typography >
-            email: s.garrison@gmail.com
+          <Typography align="center">
+            <br />
+          <Link variant="h4" href="mailto:s.garrison@gmail.com?subject=Let's Work Together!" color={"#9D0208"}>Let's get started!</Link>
           </Typography>
-          <Typography >
-            tel: 713-898-3482
-          </Typography>
+          </ThemeProvider>
         </Box>
-        </Paper>
+        
       </Grid>
     </Box>
   );
