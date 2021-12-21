954cac99884718c049f23c096f9976dcd093e188
segarrison <79775636+segarrison@users.noreply.github.com>
2021-12-20 18:12:14 -0600

    preliminary changes to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,35 +8,8 @@ import SendIcon from "@mui/icons-material";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 
-const TextBoxStyles = styled(TextField)({
-  "& label.Mui-focused": {
-    color: "purple",
-  },
-  "& .MuiInput-underline:after": {
-    borderBottomColor: "purple",
-  },
-  "& .MuiOutlinedInput-root": {
-    "& fieldset": {
-      borderColor: "dark blue",
-      //   backgroundColor: "white"
-    },
-    "&:hover fieldset": {
-      borderColor: "yellow",
-    },
-    "&.Mui-focused fieldset": {
-      borderColor: "purple",
-    },
-  },
-});
-
 function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
 
-  const handleSubmit = () => {
-    //submit the form
-  };
   return (
     <Box component="div">
       <Grid container justifyContent="center">
@@ -44,52 +17,7 @@ function Contact() {
         <Box component="form">
           
           <Typography variant="h5">Contact Me!</Typography>
-          {/* <TextBoxStyles
-              fullWidth={true}
-              label="Name"
-              variant="outlined"
-              /> */}
-          <form noValidate autoComplete="off">
-            <div>
-              <TextField
-                label="Full Name"
-                margin="normal"
-                variant="outlined"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                fullWidth
-              />
-              <TextField
-                label="Email Address"
-                margin="normal"
-                variant="outlined"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                fullWidth
-              />
-            </div>
-            <br />
-            <TextField
-              multiline
-              rows="4"
-              label="Message"
-              margin="normal"
-              variant="outlined"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              fullWidth
-            />
-            <br />
-            <Button
-              fullWidth
-              variant="contained"
-              color="secondary"
-              onClick={handleSubmit}
-            >
-              Submit
-            </Button>
-          </form>
-          <Typography variant="h5">
+          <Typography variant="h6">
             To actually get in touch with me:
           </Typography>
           <Typography >
